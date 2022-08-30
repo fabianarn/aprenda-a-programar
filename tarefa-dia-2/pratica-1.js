6c366c003320ac0b081f92b84c84a4f57d927876
fabianarn <fabiana_rn@yahoo.com>
2022-08-29 21:29:14 -0300

    pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "fabiana_rn@yahoo.com"
 let profissao = "designer"
 let idade = 45
 let viagemDosSonhos = "Tailândia"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemDosSonhos)
